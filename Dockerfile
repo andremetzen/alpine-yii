4c617614db87a28d3e385ff44e5ee99f68634b6a
Andre Metzen <andressantos@gmail.com>
2016-05-14 14:50:11 -0300

    Remove github oauth token and add base files

--- Dockerfile
+++ Dockerfile
@@ -7,11 +7,14 @@ RUN apk add --update bash curl git nginx ca-certificates \
     php-gd php-iconv php-mcrypt nodejs musl && rm -rf /var/cache/apk/*
 
 RUN curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/bin/composer
-RUN composer config -g github-oauth.github.com 4fb69fa0f79c4d0f251404898bbf3ae87d6f28d0
 RUN composer global require "fxp/composer-asset-plugin:~1.1.3"
 
 ENV PATH /root/.composer/vendor/bin:$PATH
 RUN npm install -g bower
+
+ADD files /src
+RUN cp -rf /src/* /
+RUN rm -rf /src
 
 EXPOSE 80
 
---
+++ files/start.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Function to update the fpm configuration to make the service environment variables available
+function setEnvironmentVariable {
+    if [ -z "$2" ]; then
+            echo "Environment variable '$1' not set."
+            return
+    fi
+
+    # Check whether variable already exists
+    if grep -q $1 /etc/php5/fpm/pool.d/env.conf; then
+        # Reset variable
+        sed -i "s/^env\[$1.*/env[$1] = $2/g" /etc/php5/fpm/pool.d/env.conf
+    else
+        # Add variable
+        echo "env[$1] = $2" >> /etc/php5/fpm/pool.d/env.conf
+    fi
+}
+
+function changeNginxEnvVars {
+    if [ -z "$2" ]; then
+            echo "Environment variable '$1' not set."
+            return
+    fi
+
+    echo "Replacing {$1} for $2 on /etc/nginx/sites-available/default"
+
+    sed -i "s/{$1}/$2/g" /etc/nginx/sites-available/default
+}
+
+function phpcliEnvVars {
+    if [ -z "$2" ]; then
+            echo "Environment variable '$1' not set."
+            return
+    fi
+
+    # Check whether variable already exists
+    if grep -q $1 /srv/www/config/env.php; then
+        # Reset variable
+        sed -i "s/^putenv\('$1.*/putenv('$1=$2');/g" /srv/www/config/env.php
+    else
+        # Add variable
+        echo "putenv('$1=$2');" >> /srv/www/config/env.php
+    fi
+}
+
+echo "<?php
+" > /srv/www/config/env.php
+
+
+# Grep for variables that look like MySQL (MYSQL)
+for _curVar in `env | awk -F = '{print $1}'`;do
+    # awk has split them by the equals sign
+    # Pass the name and value to our function
+    setEnvironmentVariable ${_curVar} ${!_curVar}
+    phpcliEnvVars ${_curVar} ${!_curVar}
+done
+
+for _curVar in `env | awk -F = '{print $1}'`;do
+    # awk has split them by the equals sign
+    # Pass the name and value to our function
+    changeNginxEnvVars ${_curVar} ${!_curVar}
+done
+
+# publish assets & run migrations
+/srv/www/yii migrate/up --interactive=0
+
+# start php-fpm and nginx
+cron -f &
+touch /etc/crontab
+/etc/init.d/php5-fpm start 
+exec /usr/sbin/nginx
